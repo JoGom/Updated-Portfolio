004f26ab83c308215e2190b39126586585858c0d
JoGom <jogoram428@gmail.com>
2019-03-16 03:50:05 -0700

    redesign

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,3 +6,14 @@ $(".btn").click(function() {
         scrollTop: $("."+section+"Sect").offset().top},
         1000);
 });
+
+// (function() {
+//     var menu = document.querySelector('ul'),
+//         menulink = document.querySelector('.hamburger');
+    
+//     menulink.addEventListener('click', function(e) {
+//         menu.classList.toggle('active');
+//         e.preventDefault();
+//     });
+// })();
+
