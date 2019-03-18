a51b8caaaedbb8a8b0e338d6d7f1948c476c125c
JoGom <jogoram428@gmail.com>
2019-03-17 22:16:58 -0700

    added auto scrolling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,10 @@
-$(".btn").click(function() {
+$(".nav-item").click(function() {
 
     let section = $(this).attr("data-name");
     console.log(section);
     $('html, body').animate({
-        scrollTop: $("."+section+"Sect").offset().top},
+        scrollTop: $("#"+section+"Sect").offset().top},
         1000);
 });
 
-// (function() {
-//     var menu = document.querySelector('ul'),
-//         menulink = document.querySelector('.hamburger');
-    
-//     menulink.addEventListener('click', function(e) {
-//         menu.classList.toggle('active');
-//         e.preventDefault();
-//     });
-// })();
 
